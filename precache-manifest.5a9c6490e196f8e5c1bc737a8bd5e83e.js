9eb019efa25f38317fa7cbe754ccf402a091a7b4
kowshikce <kowshik.lm14@gmail.com>
2020-10-20 19:46:02 +0600

    Updates

---
+++ precache-manifest.5a9c6490e196f8e5c1bc737a8bd5e83e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999d9c5900f5b648cefc454a26a5c998",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b22430ebc24108cba4fa",
+    "url": "/monsters-rolodex/static/css/main.5b1a559e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22430ebc24108cba4fa",
+    "url": "/monsters-rolodex/static/js/main.d2303e97.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
